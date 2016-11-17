13888f2be71602f9163c6d692a6a71f8caa967b8
Yuuon <yuuon.himemiya@gmail.com>
2016-11-17 09:27:26 +0100

    first commit

---
+++ dtw.cpp
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ *
+ * Drone control through voice recognition -- PC to drone communication
+ * Team GYTAM, feb. 2016
+ *
+ *
+ ******************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // for memcmp
+#include <stdint.h> // for int16_t and int32_t
+#include <math.h>
+#include <iostream>
+#include <limits>
+#include "dtw.h"
+using namespace std;
+/**
+* Dtw function that given two matrix of cep coefficient computes distance
+* between those two signals.
+*  @param n_ck      Dimension of unknow signal
+*  @param n_cunk    Dimension of know signal
+*  @param dim_mfcc  Size of nfcc decompostion base
+*  @param c_k       Matrix of know signal
+*  @param c_unk     Matrix of unknow signal
+*  @return Distance between the two signals
+*/
+
+float distance(float * c_k, float* c_unk, int i, int j) {
+	float d = 0;
+	//for(int k = 0; k < 12; k++) {
+    d += pow( (c_k[i]-c_unk[j]) ,2 );
+
+	//}
+	return d;
+}
+
+float dtw(int n_ck, int n_cunk, int dim_mfcc, float* c_k, float* c_unk) {
+
+/* à compléter */
+    int w0 = 1;
+    int w1 = 2;
+    int w2 = 1;
+    double r = min(n_ck, n_cunk)|2 + 1;
+    double table[n_ck][n_cunk];
+    double d;
+    table[0][0] = 0.0;
+    for(int i = 1; i < n_cunk + 1; i++ ) {
+        table[0][i] = numeric_limits<double>::infinity();
+        
+    }
+    for(int i = 1; i < n_ck + 1; i++) {
+    	table[i][0] = numeric_limits<double>::infinity();
+    }
+    for(int i = 1; i < n_ck + 1; i++ ) {
+    	for(int j = 1; j< n_cunk + 1; j++) {
+    		if(abs(i-j) <= r){
+    			float d1 = table[i-1][j] + w0 * distance(c_k, c_unk, i-1, j-1);
+    			float d2 = table[i-1][j-1] + w1 * distance(c_k, c_unk, i-1,j-1);
+    			float d3 = table[i][j-1] + w2 * distance(c_k,c_unk,i-1,j-1);
+    			table[i][j] = min(d1, min(d2,d3));
+    		} else {
+    			table[i][j] = numeric_limits<double>::infinity();
+    		}
+    	}
+    }
+    d = table[n_ck][n_cunk]/(n_ck+n_cunk);
+    return d;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <iostream>
+#include <limits>
+#include <vector>
+#include "dtw.h"
+#include "FFTReal.h"
+#include "libmfccOptim.h"
+#include "WavToMfcc.h"
+
+using namespace std;
+
+
+
+int main(){
+/*
+ * 9 fichier audio + 1 'ref.wav'
+ */
+vector<string> vocabulaire;
+vocabulaire.push_back("avance");
+vocabulaire.push_back("recule");
+vocabulaire.push_back("decollage");
+vocabulaire.push_back("droite");
+vocabulaire.push_back("gauche");
+vocabulaire.push_back("tournedroite");
+vocabulaire.push_back("tournegauche");
+vocabulaire.push_back("faisunflip");
+vocabulaire.push_back("arretetoi");
+vocabulaire.push_back("etatdurgence");
+
+
+string chemin = "";
+
+int nbmots = vocabulaire.size();
+const char *nomFichier;
+char *ref; 
+string fichier; 
+float table [9] ;
+int n_ck, n_cunk, dim_mfcc;
+float * c_k;
+float * c_unk;
+/*lecture des fichiers audio*/
+for(int mot = 1; mot < nbmots; mot++){
+	fichier = chemin + vocabulaire[mot] + ".wav";
+	nomFichier = fichier.c_str();
+	char * nomF = strdup(nomFichier);
+	char * mfcName = (char *)malloc(256 * sizeof(char));
+	strcpy(mfcName, nomF);
+	nameWavToMfc(nomF, mfcName);
+	FILE **audioFile;
+	wavfile * pHeader;
+	wavRead(audioFile, nomF, pHeader);
+	/*
+	 * TODO:
+	 * il faut faire une lecteure sur le fichier ouvert par WavRead
+	 * et le mettre dans un tableau de int16
+	 * i.e.: while(fread(&buf,sizeof(int16_t),1,p_wav)>0)
+	 *       x[i++] = buf;
+	 * Pour computeMFCC, il s'agit de :
+	 * frame_length : la taille de la fenetre de calcul des parametres.
+	 * Par exemple 512 échantillons.
+	 *
+	 * frame_step : correspond au décallage déchantillons
+	 * pour prendre la fenetre suivante.
+	 * Si on veut prendre la moitié, prendre 256.
+	 *
+	 * dim_mfcc : en général 13. C'est un max pour de la parole.
+	 *  ************** on prends 12 comme MATLAB **************
+	 *
+	 *  num_filter : Prende 26 par exemple.
+	 *
+	 *  ************* s'il y a probleme executer(compiler OK),
+	 *                essayer initialiser les pointeur ********
+	 */
+
+	dtw(n_ck, n_cunk, dim_mfcc, c_k, c_unk);
+}
+
+
+
+/*Compute MFCC of a signal*/
+//computeMFCC(float **X_mfcc, int *length_xmfcc, int16_t *x, int Nx, int frequency, int frame_length, int frame_step, int dim_mfcc, int num_filter);
+
+/*Remove silence of the signal at the start and the end*/
+//removeSilence(int16_t *x, int Nx, int16_t **xFiltered, int *newLength, float threshold);
+
+/*Read a wave file.*/
+//wavRead(FILE **p_wav, char *filename, wavfile *p_header); 
+
+/*il faut recuperer le fichier de l'app et le comparer avec les fichiers que lui reconnait*/
+//dtw(int n_ck, int n_cunk, int dim_mfcc, float * c_k, float * c_unk);
+
+}
